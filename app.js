71c4fc4835bdf72aaedf92c16f2f250cdfc41613
rohankhera1 <68718020+rohankhera1@users.noreply.github.com>
2021-01-31 10:24:04 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,200 @@
+function initMap() {
+  // Create the map.
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {lat: 52.632469, lng: -1.689423},
+  });
+
+  // Load the vaccine locations GeoJSON onto the map.
+  map.data.loadGeoJson('vaccines.json', {idPropertyName: 'vaccineid'});
+
+  const apiKey = 'AIzaSyDzByzQVTI35JAIShkspVEqQyoTl5rV5lY';
+  const infoWindow = new google.maps.InfoWindow();
+
+  // Show the information for a vaccine when its marker is clicked.
+  map.data.addListener('click', (event) => {
+    const category = event.feature.getProperty('category');
+    const name = event.feature.getProperty('name');
+    const remaining_vaccines = event.feature.getProperty('remaining_vaccines');
+    const hours = event.feature.getProperty('hours');
+    const form = event.feature.getProperty('form');
+    const position = event.feature.getGeometry().get();
+    const content = `
+      <h2>${name}</h2><p>${remaining_vaccines}</p>
+      <p><b>Open:</b> ${hours}<br/><b>Form:</b> ${form}</p>
+    `;
+
+    infoWindow.setContent(content);
+    infoWindow.setPosition(position);
+    infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
+    infoWindow.open(map);
+  });
+
+  // Build and add the search bar
+  const card = document.createElement('div');
+  const titleBar = document.createElement('div');
+  const title = document.createElement('div');
+  const container = document.createElement('div');
+  const input = document.createElement('input');
+  const options = {
+    types: ['address'],
+  };
+
+  card.setAttribute('id', 'pac-card');
+  title.setAttribute('id', 'title');
+  title.textContent = 'Find the nearest vaccine';
+  titleBar.appendChild(title);
+  container.setAttribute('id', 'pac-container');
+  input.setAttribute('id', 'pac-input');
+  input.setAttribute('type', 'text');
+  input.setAttribute('placeholder', 'Enter an address');
+  container.appendChild(input);
+  card.appendChild(titleBar);
+  card.appendChild(container);
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+  // Make the search bar into a Places Autocomplete search bar and select
+  // which detail fields should be returned about the place that
+  // the user selects from the suggestions.
+  const autocomplete = new google.maps.places.Autocomplete(input, options);
+
+  autocomplete.setFields(
+      ['address_components', 'geometry', 'name']);
+
+  // Set the origin point when the user selects an address
+  const originMarker = new google.maps.Marker({map: map});
+  originMarker.setVisible(false);
+  let originLocation = map.getCenter();
+
+  autocomplete.addListener('place_changed', async () => {
+    originMarker.setVisible(false);
+    originLocation = map.getCenter();
+    const place = autocomplete.getPlace();
+
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert('No address available for input: \'' + place.name + '\'');
+      return;
+    }
+
+    // Recenter the map to the selected address
+    originLocation = place.geometry.location;
+    map.setCenter(originLocation);
+    map.setZoom(9);
+    console.log(place);
+
+    originMarker.setPosition(originLocation);
+    originMarker.setVisible(true);
+
+    // Use the selected address as the origin to calculate distances
+    // to each of the vaccine locations
+    const rankedVaccines = await calculateDistances(map.data, originLocation);
+    showVaccinesList(map.data, rankedVaccines);
+
+    return;
+  });
+
+}
+
+async function calculateDistances(data, origin) {
+  const vaccines = [];
+  const destinations = [];
+
+  // Build parallel arrays for the vaccine IDs and destinations
+  data.forEach((vaccine) => {
+    const vaccineNum = vaccine.getProperty('vaccineid');
+    const vaccineLoc = vaccine.getGeometry().get();
+
+    vaccines.push(vaccineNum);
+    destinations.push(vaccineLoc);
+  });
+
+  // Retrieve the distances of each vaccine from the origin
+  // The returned list will be in the same order as the destinations list
+  const service = new google.maps.DistanceMatrixService();
+  const getDistanceMatrix =
+    (service, parameters) => new Promise((resolve, reject) => {
+      service.getDistanceMatrix(parameters, (response, status) => {
+        if (status != google.maps.DistanceMatrixStatus.OK) {
+          reject(response);
+        } else {
+          const distances = [];
+          const results = response.rows[0].elements;
+          for (let j = 0; j < results.length; j++) {
+            const element = results[j];
+            const distanceText = element.distance.text;
+            const distanceVal = element.distance.value;
+            const distanceObject = {
+              vaccineid: vaccines[j],
+              distanceText: distanceText,
+              distanceVal: distanceVal,
+            };
+            distances.push(distanceObject);
+          }
+
+          resolve(distances);
+        }
+      });
+    });
+
+  const distancesList = await getDistanceMatrix(service, {
+    origins: [origin],
+    destinations: destinations,
+    travelMode: 'DRIVING',
+    unitSystem: google.maps.UnitSystem.METRIC,
+  });
+
+  distancesList.sort((first, second) => {
+    return first.distanceVal - second.distanceVal;
+  });
+
+  return distancesList;
+}
+
+function showVaccinesList(data, vaccines) {
+  if (vaccines.length == 0) {
+    console.log('empty vaccines');
+    return;
+  }
+
+  let panel = document.createElement('div');
+  // If the panel already exists, use it. Else, create it and add to the page.
+  if (document.getElementById('panel')) {
+    panel = document.getElementById('panel');
+    // If panel is already open, close it
+    if (panel.classList.contains('open')) {
+      panel.classList.remove('open');
+    }
+  } else {
+    panel.setAttribute('id', 'panel');
+    const body = document.body;
+    body.insertBefore(panel, body.childNodes[0]);
+  }
+
+
+  // Clear the previous details
+  while (panel.lastChild) {
+    panel.removeChild(panel.lastChild);
+  }
+
+  vaccines.forEach((vaccine) => {
+    // Add vaccine details with text formatting
+    const name = document.createElement('p');
+    name.classList.add('place');
+    const currentVaccine = data.getFeatureById(vaccine.vaccineid);
+    name.textContent = currentVaccine.getProperty('name');
+    panel.appendChild(name);
+    const distanceText = document.createElement('p');
+    distanceText.classList.add('distanceText');
+    distanceText.textContent = vaccine.distanceText;
+    panel.appendChild(distanceText);
+  });
+
+  // Open the panel
+  panel.classList.add('open');
+
+  return;
+
+
+}
